2eeef8352afe5655b5130e33933a7c1b1d42dc37
Mohamed-mekawy <mekawy2012@gmail.com>
2015-09-25 11:30:29 -0700

    1.2 Add database test Function

--- app/src/main/java/com/example/mekawy/cprovider/MainFragActivity.java
+++ app/src/main/java/com/example/mekawy/cprovider/MainFragActivity.java
@@ -1,5 +1,7 @@
 package com.example.mekawy.cprovider;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.provider.ContactsContract;
 import android.support.v7.app.ActionBarActivity;
@@ -10,7 +12,7 @@ import android.view.MenuItem;
 
 import com.example.mekawy.cprovider.Data.dbContract;
 import com.example.mekawy.cprovider.Data.dbOpenhelper;
-
+import com.example.mekawy.cprovider.Data.dbContract.POP_MOVIES_TABLE;
 
 public class MainFragActivity extends ActionBarActivity {
 
@@ -18,8 +20,38 @@ public class MainFragActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_frag);
+        test_db_tabels();
     }
 
+
+
+    public void test_db_tabels(){
+        dbOpenhelper mhelper=new dbOpenhelper(getApplicationContext());
+        SQLiteDatabase db =mhelper.getWritableDatabase();
+        Long value;
+
+        //Content values
+        ContentValues mContent=new ContentValues();
+
+        mContent.put(POP_MOVIES_TABLE.OWM_COLUMN_TAG,561330);
+        mContent.put(POP_MOVIES_TABLE.OWM_COLUMN_TITLE,"movie name");
+        mContent.put(POP_MOVIES_TABLE.OWM_COLUMN_OVERVIEW,"overview");
+        mContent.put(POP_MOVIES_TABLE.OWM_COLUMN_RELEASE_DATE,"2015");
+        mContent.put(POP_MOVIES_TABLE.OWM_COLUMN_POSTER_PATH,"poster path");
+        mContent.put(POP_MOVIES_TABLE.OWM_COLUMN_VOTE_AVERAGE,21.5);
+        mContent.put(POP_MOVIES_TABLE.OWM_COLUMN_IS_FAVORITE,1);// movie is in favorite list
+
+        value=db.insert(POP_MOVIES_TABLE.TABLE_NAME,null,mContent);
+        Log.i("INSERT INTO POP", Long.toString(value));
+
+        value=db.insert(dbContract.MOST_VOTED_TABLE.TABLE_NAME,null,mContent);
+        Log.i("INSERT INTO VOTE",Long.toString(value));
+
+        mContent.remove(POP_MOVIES_TABLE.OWM_COLUMN_IS_FAVORITE);
+
+        value=db.insert(dbContract.FAV_MOVIES.TABLE_NAME,null,mContent);
+        Log.i("INSERT INTO FAV", Long.toString(value));
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
