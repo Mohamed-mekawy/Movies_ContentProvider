32cd9e402862d89a1cab29a45fff970309a6bf2c
Mohamed-mekawy <mekawy2012@gmail.com>
2015-09-25 10:09:53 -0700

    1.0 DB Contract  containing three Tables

---
+++ app/src/main/java/com/example/mekawy/cprovider/MainFragActivityFragment.java
@@ -0,0 +1,23 @@
+package com.example.mekawy.cprovider;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainFragActivityFragment extends Fragment {
+
+    public MainFragActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
